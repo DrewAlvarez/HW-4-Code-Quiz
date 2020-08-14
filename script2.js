7fb01d8c642e9e039b42e2323d45808eee784b69
Andrew <alvarez.drew@gmail.com>
2020-08-14 14:39:47 -0500

    reworked js, some css, tring to get score counter working.

--- script2.js
+++ script2.js
@@ -1,107 +1,90 @@
 var startButton = document.querySelector("#start");
+var startText = document.querySelector("#quizStartText")
 var timer = document.querySelector("#timer");
-var questionBox = document.querySelector("#questions");
-var answer1 = document.querySelector("#answer1");
-var answer2 = document.querySelector("#answer2");
-var answer3 = document.querySelector("#answer3");
-var answer4 = document.querySelector("#answer4");
-var increment = 0;
+var questionEl = document.createElement("h3");
+var quizBox = document.querySelector("#quiz-area")
+var idx = 0;
 var score = 0;
 
 function start(){
-    questionBox.textContent = "The following quiz is timed, answer correctly to obtain a high score. Correct answers award 5 points. Wrong answers will subtract 1 point and reduce your time by 5 seconds. Must choose the correct answer to advance."
+    startText.textContent = "The following quiz is timed, answer correctly to obtain a high score. Correct answers award 5 points. Wrong answers will reduce your time by 5 seconds. Must choose the correct answer to advance."
     startButton.textContent = "Click her to start";
-    answer1.disabled = true;
-    answer2.disabled = true;
-    answer3.disabled = true;
-    answer4.disabled = true;
-    return;
 }
 start();
 
 var questions = [
     {
-        question: "This is question 1?",
-        answers: ["ans 1.1","ans 1.2", "ans 1.3","ans 1.4"],
-        correctAnswer: "ans 1.2"
+        question: "What does HTML stand for?",
+        answers: ["Hypertype Median Language","Hypertext Markup Language", "Hypertext Modular Lines","Nothing"],
+        correctAnswer: "Hypertext Markup Language"
     },
     {
-        question: "This is question 2?",
-        answers: ["ans 2.1","ans 2.2", "ans 2.3","ans 2.4"],
-        correctAnswer: "ans 2.1"
+        question: "Which one of the Following is not a 'Semantic Element' in HTML?",
+        answers: ["<object>","<footer>", "<main>","<artcle>"],
+        correctAnswer: "<object>"
     },
     {
-        question: "This is question 3?",
-        answers: ["ans 3.1","ans 3.2", "ans 3.3","ans 3.4"],
-        correctAnswer: "ans 3.3"
+        question: "CSS styling is written in which element of an HTML document?",
+        answers: ["<body>", "<div>", "<head>","<footer>"],
+        correctAnswer: "<head>"
     },
     {
-        question: "This is question 4?",
-        answers: ["ans 4.1","ans 4.2", "ans 4.3","ans 4.4"],
-        correctAnswer: "ans 4.4"
+        question: "In the CSS Box Model, which of the following is the outer most part of the box?",
+        answers: ["border","content", "padding","margin"],
+        correctAnswer: "margin"
     },
     {
-        question: "This is question 5?",
-        answers: ["ans 5.1","ans 5.2", "ans 5.3","ans 5.4"],
-        correctAnswer: "ans 5.3"
+        question: "In JavaScript, if you create an element and you want add it to your HTML, you must ____/____ it.",
+        answers: ["push/pull","attach/affix", "append/prepend","adjoin/join"],
+        correctAnswer: "append/prepend"
     }
 ]
 
 function questionFunc(){
     startButton.style.display = "none";
-    answer1.disabled = false;
-    answer2.disabled = false;
-    answer3.disabled = false;
-    answer4.disabled = false;
-    questionBox.textContent = questions[increment].question;
-    answer1.textContent = questions[increment].answers[0]
-    answer2.textContent = questions[increment].answers[1]
-    answer3.textContent = questions[increment].answers[2]
-    answer4.textContent = questions[increment].answers[3]
+    startText.style.display = "none";
+    questionEl.textContent = questions[idx].question;
+    quizBox.appendChild(questionEl);
     
-    answer1.addEventListener("click", function(){
-        var choice = this.textContent
-        if (choice === questions[increment].correctAnswer){
-            score = score + 5;
-            increment++;
-            questionFunc();
-        }else{
-            remainingSeconds = remainingSeconds - 5;
-        }
-    })
-    answer2.addEventListener("click", function(){
-        var choice = this.textContent
-        if (choice === questions[increment].correctAnswer){
-            score = score + 5;
-            increment++;
-            questionFunc();
-        }else{
-            remainingSeconds = remainingSeconds - 5;
-        }
-    })
-    answer3.addEventListener("click", function(){
-        var choice = this.textContent
-        if (choice === questions[increment].correctAnswer){
-            score = score + 5;
-            increment++;
-            questionFunc();
-        }else{
-            remainingSeconds = remainingSeconds - 5;
-        }
-    })
-    answer4.addEventListener("click", function(){
-        var choice = this.textContent
-        if (choice === questions[increment].correctAnswer){
-            score = score + 5;
-            increment++;
-            questionFunc();
-        }else{
-            remainingSeconds = remainingSeconds - 5;
-        }
-    })
-
+    for(i=0; i < questions[idx].answers.length; i++){
+        var answerChoice = document.createElement("button");
+        answerChoice.setAttribute("class", "btn btn-info btn-lg btn-block");
     
+        answerChoice.addEventListener("click", function(){
+            var choice = this.textContent
+            if (choice === questions[idx].correctAnswer){
+                quizBox.innerHTML = "";
+                score = score + 5;
+                console.log(score)
+                idx++;
+                if (idx<5){
+                    questionFunc();
+                }else{
+                    endQuiz()
+                }
+            }else{
+                quizBox.innerHTML = "";
+                remainingSeconds = remainingSeconds - 5;
+                idx++;
+                if (idx<5){
+                    questionFunc();
+                }else{
+                    endQuiz()
+                }
+            }
 
+        });
+        answerChoice.textContent = questions[idx].answers[i] + "\n";
+        quizBox.append(answerChoice);
+    }
+    return;
+}
+
+function endQuiz(){
+    quizBox.innerHTML = "";
+    questionEl.textContent = "Congradulations! Your score is " + score + ". Please enter your initials."
+    quizBox.appendChild(questionEl);
+    return;
 }
 
 var remainingSeconds = 60;
@@ -114,28 +97,15 @@ function startQuiz() {
         if (remainingSeconds < 0){
             clearInterval(timerId);
             timer.textContent = 0;
-            setTimeout(function(){
-                //endscreen
-            })
+
         }
         if (remainingSeconds < 6){
             document.querySelector("#timer-bg").style.backgroundColor = "red";
         }
     }, 1000)
 
-
-    // function tester(){
-    //     answer2.disabled = false;
-    //     answer3.disabled = false;
-    //     answer4.disabled = false;
-    //     questionBox.textContent = "hello";
-    //     answer1.textContent = "0";
-    //     answer2.textContent = "1";
-    //     answer3.textContent = "2";
-    //     answer4.textContent = "3";
-    //     return;
-    // }
     questionFunc()
+
 
 }
 
